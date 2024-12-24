c308770dcac2e7b98edfa831f09703ed19438cfd
David Cho <happerroser2@gmail.com>
2024-12-25 02:18:03 +0900

    1655 done

--- 1XXX/1655.cpp
+++ 1XXX/1655.cpp
@@ -1,15 +1,40 @@
 // https://www.acmicpc.net/problem/1655
+//참고
+//https://www.acmicpc.net/blog/view/56
+//Use scanf and printf instead of cin and cout to save time
 #include <iostream>
+#include <queue>
 #include <vector>
-#include <algorithm>
 
 int main()
 {
     int n;
+    int t;
     std::cin >> n;
+    std::priority_queue<int> leftHeap;
+    std::priority_queue<int, std::vector<int>, std::greater<int> > rightHeap;
+    std::vector<int> result(n);
 
     for (int i = 0; i < n; i++)
     {
+        scanf("%d",&t);
+        if (leftHeap.empty() || t <= leftHeap.top())
+        {
+            leftHeap.push(t);
+        }
+        else if (t > leftHeap.top())
+        {
+            rightHeap.push(t);
+        }
+
+        if(leftHeap.size() >= rightHeap.size() + 2){
+            rightHeap.push(leftHeap.top());
+            leftHeap.pop();
+        } else if(leftHeap.size() < rightHeap.size()){
+            leftHeap.push(rightHeap.top());
+            rightHeap.pop();
+        }
+        printf("%d\n",leftHeap.top());
     }
     return 0;
 }
