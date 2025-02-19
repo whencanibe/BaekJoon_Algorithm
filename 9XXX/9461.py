66de2eefae695e10b8be6e48f053cb69ef855673
David Cho <happerroser2@gmail.com>
2025-02-20 01:19:51 +0900

    9461 dp

--- 9XXX/9461.py
+++ 9XXX/9461.py
@@ -2,7 +2,21 @@ def solve():
     import sys
     input = sys.stdin.readline
 
-    
+    T = int(input().strip())
+
+    dp = [0] * (101)
+
+    dp[1]= dp[2] = dp[3] = 1
+
+    for i in range(4 ,101):
+        dp[i] = dp[i - 2] + dp[i - 3]
+
+
+    for _ in range(T):
+        N = int(input().strip())
+        print(dp[N])
+
+
 
 
 if __name__ == "__main__":
