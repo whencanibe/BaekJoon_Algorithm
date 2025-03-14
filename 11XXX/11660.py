aa8f40504f9c3376b93399092b2e13fbd64042f6
David Cho <happerroser2@gmail.com>
2025-03-14 13:13:10 +0900

    11660 finished

--- 11XXX/11660.py
+++ 11XXX/11660.py
@@ -1,25 +1,28 @@
-def solve():
-    import sys
-    input = sys.stdin.readline
+import sys
+readline = sys.stdin.readline
+def solve():    
+    N, M = map(int , readline().split())
 
-    grid = []
-    N, M = map(int , input().strip())
+    grid = [list(map(int, readline().split())) for _ in range(N)]
 
-    for i in range(N):
-        row = list(map(int, input().split()))
-        grid.append(row)
+    dp = [[0] * (N+1) for _ in range(N+1)] # 1-based 
 
-    dp = [[0] * (N + 1) for _ in range(N + 1)]
-
-    for i in range(1 ,N+1):
-        for j in range(1, N+1):
-            dp[i][j] = dp[i][j-1] + grid[i][j]
+    for i in range(1, N+1):
+        for j in range(1 ,N+1):
+            dp[i][j] = grid[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+    
+    result = []
+    for i in range(M):
+        # x, y 행-열 문제 잘 읽기
+        x1, y1, x2, y2 = map(int, readline().split())
+        
+        tmp = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
+        result.append(tmp)
+            
+    for res in result:
+        print(res)
             
     
-    for j in range(M):
-        x1,y1,x2,y2 = map(int, input().split())
-
-
 
 if __name__ == "__main__":
     solve()
